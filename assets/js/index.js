ec0c9b9e6d7f9d3c31e8151b79cff21261c46509
Sophie Koonin <sophie.koonin@gmail.com>
2019-11-02 15:19:08 +0000

    some CSS Magic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 function toggleHaunting() {
   const ghost = document.getElementById('ghost');
-  ghost.className === 'haunt'
-    ? (ghost.className = '')
-    : (ghost.className = 'haunt');
+  ghost.className = '';
+  void ghost.offsetWidth;
+  ghost.className = 'haunt';
 }
