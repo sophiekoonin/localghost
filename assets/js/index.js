f9593f84a3bd383b44efef1eeeae4b64e11da478
Sophie Koonin <sophie@localghost.dev>
2020-07-11 14:00:30 +0100

    Scripts for dark mode:

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,88 +1,59 @@
-// https://codepen.io/DirkWeber/pen/eyxbmq
+document.documentElement.classList.remove('no-js');
 
-(function() {
-  var ua = navigator.userAgent;
+// thx Andy Bell: https://hankchizljaw.com/wrote/create-a-user-controlled-dark-or-light-mode/
+const STORAGE_KEY = 'user-color-scheme';
+const COLOR_MODE_KEY = '--color-mode';
 
-  if (!/firefox/gi.test(ua)) {
-    return;
-  }
-  // Browsersniffing. Yes, I'm lazy.
+const darkModeCheckbox = document.querySelector('#toggle-checkbox');
+const getCSSCustomProp = (propKey) => {
+  let response = getComputedStyle(document.documentElement).getPropertyValue(
+    propKey
+  );
 
-  var fragmentID;
-  var feImgs = document.querySelectorAll('feImage');
-  var cssList = document.styleSheets;
-  var cssAnimations = {};
-
-  for (var i = 0; i < cssList.length; i++) {
-    var css = cssList[i];
-
-    for (var j = 0; j < css.cssRules.length; j++) {
-      var rule = css.cssRules[j];
-
-      //is rule a keyframe animation?
-      if (rule.type === 7) {
-        cssAnimations[rule.name] = rule.cssText;
-      }
-    }
+  if (response.length) {
+    response = response.replace(/\"/g, '').trim();
   }
 
-  for (var i = 0, j = feImgs.length; i < j; i++) {
-    fragmentID =
-      feImgs[i].getAttribute('xlink:href') || feImgs[i].getAttribute('xlink');
+  return response;
+};
 
-    if (/#/.test(fragmentID) && !/data\:image\/svg\+xml/.test(fragmentID)) {
-      fragmentAsURIintoFilter(fragmentID, feImgs[i]);
-    }
-  }
+const applySetting = (passedSetting) => {
+  let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
 
-  function fragmentAsURIintoFilter(identifier, fePrimitive) {
-    var el = document.querySelector(identifier);
-    var styledEl = inlineStyles(el);
-    var nsAttr = 'http://www.w3.org/2000/svg';
-    if (!styledEl.getAttribute('xmlns')) {
-      styledEl.setAttribute('xmlns', nsAttr);
-    }
-    var text = encodeURIComponent(
-      styledEl.outerHTML.replace(/100%/g, '256')
-    ).replace('"', "'");
-    var target = fePrimitive;
-
-    target.setAttribute(
-      'xlink:href',
-      'data:image/svg+xml;charset=utf-8,' + text
+  if (currentSetting) {
+    document.documentElement.setAttribute(
+      'data-user-color-scheme',
+      currentSetting
     );
+  } else {
+    currentSetting = getCSSCustomProp(COLOR_MODE_KEY);
+  }
+  darkModeCheckbox.checked = currentSetting === 'dark';
+};
+
+const toggleSetting = () => {
+  let currentSetting = localStorage.getItem(STORAGE_KEY);
+
+  switch (currentSetting) {
+    case null:
+      currentSetting =
+        getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
+      break;
+    case 'light':
+      currentSetting = 'dark';
+      break;
+    case 'dark':
+      currentSetting = 'light';
+      break;
   }
 
-  // When URLencoded, global styles will no longer apply to the SVG Fragment. So we must inline every rule before:
-  function inlineStyles(element) {
-    var children = element.querySelectorAll('*');
-    var animations = [];
+  localStorage.setItem(STORAGE_KEY, currentSetting);
 
-    [].forEach.call(children, function(child) {
-      var style = getComputedStyle(child);
-      child.cssText = style;
+  return currentSetting;
+};
 
-      // Look for animations:
-      for (var i = 0; i < style.length; i++) {
-        var prop = style.item(i);
-        var rule = style.getPropertyValue(prop);
+darkModeCheckbox.addEventListener('click', (evt) => {
+  applySetting(toggleSetting());
+});
 
-        // is there a CSS keyframe animation applied to this element?
-        // Copy it to an inlined style.
-        if (/animation\-name/.test(prop) && rule !== 'none') {
-          if (animations.toString().indexOf(rule) < 0) {
-            var svgStyle = document.createElement('style');
-
-            svgStyle.innerHTML = cssAnimations[rule];
-            element.appendChild(svgStyle);
-            animations.push(rule);
-          }
-        }
-
-        child.style[prop] = rule;
-      }
-    });
-
-    return element;
-  }
-})();
+applySetting();
