dbab6d242b95cb52d8958f3293c0dcb3c99f5112
Sophie Koonin <sophie@localghost.dev>
2024-02-04 10:54:46 +0000

    fix up links

--- scripts/compile-links.js
+++ scripts/compile-links.js
@@ -5,14 +5,14 @@ const fs = require("fs");
 const collectionId = process.env.RAINDROP_COLLECTION_ID;
 const token = process.env.RAINDROP_TOKEN;
 const today = new Date();
-const lastSaturday = subDays(today, 8);
-const formattedLastSaturday = format(lastSaturday, "yyyy-MM-dd");
+const lastSaturday = subDays(today, 7);
+const formattedLastSunday = format(lastSaturday, "yyyy-MM-dd");
 const formattedToday = format(today, "yyyy-MM-dd");
 
 async function fetchLinks() {
   // Get content bookmarked between last Sunday and this Saturday inclusive
   const search = new URLSearchParams({
-    search: `created:>${formattedLastSaturday} created:<${formattedToday}`,
+    search: `created:>${formattedLastSunday} created:<${formattedToday}`,
   });
   const url = new URL(`https://api.raindrop.io/rest/v1/raindrops/${collectionId}`);
   url.search = search;
@@ -35,11 +35,17 @@ function writePost(raindrops) {
   let postContent = fs.readFileSync("./scripts/link_template.md", "utf8");
   postContent = postContent.replace("{{date}}", formattedToday);
   postContent = postContent.replace("{{links}}", formattedLinks.join("\n"));
+  const filename = `./src/blog/links/${formattedToday}.md`;
+  if (process.env.DEBUG) {
+    console.log(postContent);
+    return;
+  }
   fs.writeFileSync(`./src/blog/links/${formattedToday}.md`, postContent);
 }
 
 async function main() {
   fetchLinks().then((res) => {
+    debugger;
     if (res.items.length === 0) {
       console.log("No links found, exiting");
       return;
